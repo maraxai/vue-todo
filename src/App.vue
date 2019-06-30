482d2c48af397e3ea808bc399c3c7e675625d676
Mike Dietz <mikedietz@hotmail.com>
2019-06-30 13:00:34 +0200

    basic file setup

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <md-field>
+      <md-input v-model="currentTodo" @keydown.enter="addTodo()" placeholder="Add a todo"></md-input>
+    </md-field>
+    <ul class="todos">
+      <li v-for="todo in todos" :key="todo.id">
+        {{ todo.label }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        todos: [],
+        currentTodo: ''
+      };
+    },
+    methods: {
+      addTodo() {
+        this.todos.push({id: this.todos.length, label: this.currentTodo, completed: false});
+        this.currentTodo = '';
+      }
+    }
+  }
+</script>
+
+<style>
+</style>
