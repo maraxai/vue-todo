356c3c48d34d1253e03d77ff19a8b482137bdf46
Mike Dietz <mikedietz@hotmail.com>
2019-06-30 14:15:58 +0200

    add Remove, Edit, Clear List

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,136 @@
 <template>
   <div>
-    <md-field>
-      <md-input v-model="currentTodo" @keydown.enter="addTodo()" placeholder="Add a todo"></md-input>
-    </md-field>
-    <ul class="todos">
-      <li v-for="todo in todos" :key="todo.id">
-        {{ todo.label }}
-      </li>
-    </ul>
+    <h1 class="title">Your to-do list:</h1>
+    <md-field class="addToList">
+      <md-button @click="addTodo()" class="md-icon-button addItem">
+        <md-icon>add</md-icon>
+      </md-button>
+      <md-input
+        class="add"
+        v-model="currentTodo"
+        @keyup.enter="addTodo()"
+        placeholder="to-do..."
+      ></md-input>
+    </md-field class="clearList">
+    <md-list class="todos">
+      <md-list-item
+        v-for="(todo, index) in todos"
+        :key="todo.id"
+        @dblclick="editTodo(index)"
+        class="todo"
+        :class="{completed: todo.completed}"
+      >
+        <md-checkbox
+          v-model="boolean"
+          value="todo.completed"
+          @change="completeTodo(todo)"
+        >
+          <label v-if="!todo.edit">{{ todo.label }}</label>
+          <input
+            class="edit"
+            type="text"
+            v-model="todo.label"
+            v-if="todo.edit"
+            v-focus
+            @blur="doneEdit(index)"
+            @keyup.enter="doneEdit(index)"
+          >
+        </md-checkbox>
+        <md-button class="remove" @click="removeTodo(todo)">remove</md-button>
+        <br>
+        <br>
+      </md-list-item>
+    </md-list>
+    <footer v-show="todos.length > 0" class="clearList">
+      <md-button class="clear" @click="removeAll()">Clear list</md-button><br>
+      <md-button class="clear" @click="removeCompleted()">Clear completed</md-button>
+    </footer>
   </div>
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        todos: [],
-        currentTodo: ''
-      };
-    },
-    methods: {
-      addTodo() {
-        this.todos.push({id: this.todos.length, label: this.currentTodo, completed: false});
-        this.currentTodo = '';
+const focus = {
+  inserted(el) {
+    el.focus();
+  }
+};
+export default {
+  data() {
+    return {
+      todos: [],
+      currentTodo: ""
+    };
+  },
+  directives: { focus },
+  methods: {
+    addTodo() {
+      if (this.currentTodo !== "") {
+        this.todos.push({
+          id: this.todos.length,
+          label: this.currentTodo,
+          completed: false,
+          edit: false
+        });
       }
+      this.currentTodo = "";
+    },
+    completeTodo(todo) {
+      if (todo.completed === true) {
+        todo.completed = false;
+      }
+      if (todo.completed === false) {
+        todo.completed = true;
+      }
+    },
+    editTodo(index) {
+      this.todos[index].edit = true;
+    },
+    doneEdit(index) {
+      this.todos[index].edit = false;
+    },
+    removeTodo(todo) {
+      var index = this.todos.indexOf(todo);
+      this.todos.splice(index, 1);
+    },
+    removeAll() {
+      this.todos = [];
+    },
+    removeCompleted() {
+      this.todos = this.todos.filter(function(todo) {
+        return !todo.completed;
+      });
     }
   }
+};
 </script>
 
 <style>
+body {
+  padding: 20px;
+  background-color: #c9c9c9;
+}
+
+.remove {
+  background-color: white;
+  padding: 0.25em;
+}
+
+.addToList {
+  background-color: white;
+}
+
+.clearList {
+  background-color: white;
+}
+
+.todos {
+display: flex;
+flex-direction: column;
+justify-content: center;
+}
+
+.md-checkbox {
+    display: flex;
+  }
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
-import { MdField } from 'vue-material/dist/components';
+import { MdField, MdCheckbox } from 'vue-material/dist/components';
 import 'vue-material/dist/vue-material.min.css';
 
-Vue.use(MdField);
+Vue.use(MdField, MdCheckbox);
 
 Vue.config.productionTip = false
 
